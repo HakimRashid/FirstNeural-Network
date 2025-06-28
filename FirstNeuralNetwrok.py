ae25e847f59c9a2e3fca40212b409769c0e0599b
HakimRashid <rashidhakim514@gmail.com>
2025-06-28 01:07:59 -0700

    first commit

---
+++ FirstNeuralNetwrok.py
@@ -0,0 +1,29 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+weights = [[2.1, 5.4],
+            [3.1, 6.4],
+            [7.8, 9.1],
+            [4, 0.4]]
+output_weights = np.array([4.1, 2.1, 9.2, 7.8])
+bias = np.array([1, 2, 3, 4])
+inputs = [175, 100]
+target = 0
+
+#forwardpass begins here
+def sigmoid(x: float):
+        return 1/(1+np.exp(-x))
+
+z_hidden = np.dot(weights, inputs) + bias
+outputs_H = np.array([sigmoid(x) for x in z_hidden])
+
+z_output = np.dot(output_weights, outputs_H) + 5.1
+prediction = sigmoid(z_output)
+
+#backpropagation starts here
+loss = -(target*np.log(prediction) + (1-target)*np.log(1-prediction))
+
+blame = [(prediction - target) * prediction * (1 - prediction) * weight * output * (1 - output) for weight, output in zip(output_weights, outputs_H)]
+
+print(prediction)
