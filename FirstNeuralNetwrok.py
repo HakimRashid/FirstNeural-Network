Learned: -How to dynamically update weights '.npz' files -fixed dimension issues in final few backpropagation lines - renamed variables to avoid cofusion -loss calculation (leaving this and matlib for future visualization of the training process)

FirstNeuralNetwrok.py
@@ -1,13 +1,22 @@
-import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-weights = np.array([[2.1, 5.4],
-            [3.1, 6.4],
-            [7.8, 9.1],
-            [4, 0.4]])
-output_weights = np.array([4.1, 2.1, 9.2, 7.8])
-bias = np.array([1, 2, 3, 4])
+#file loading logic 
+if not os.path.exists('weights.npz'):
+    weights = np.array([[2.1, 5.4],
+                        [3.1, 6.4],
+                        [7.8, 9.1],
+                        [4, 0.4]])
+    output_weights = np.array([4.1, 2.1, 9.2, 7.8])
+    bias = np.array([1, 2, 3, 4])
+    np.savez('weights.npz', weights=weights, output_weights=output_weights, bias=bias)
+data = np.load('weights.npz')
+weights = data['weights']
+output_weights = data['output_weights']
+bias = data['bias']
+
+
 inputs = np.array([[180.89, 70.83],
         [165.23, 47.93],
         [172.55, 60.19],
@@ -26,24 +35,18 @@ training = False
 def sigmoid(x: float):
         return 1/(1+np.exp(-x))
 
-if inputs.shape[0] != weights.shape[1]:
-        z_hidden = np.dot(inputs, weights.T) + bias
-else:
-        z_hidden = np.dot(weights, inputs) + bias
+z_hidden = np.dot(inputs, weights.T) + bias
 
 outputs_H = np.array(sigmoid(z_hidden))
 
-if outputs_H.shape[0] != output_weights.shape[1]:
-        z_output = np.dot(outputs_H, output_weights.T) + 5.1
-else:
-        z_output = np.dot(output_weights, outputs_H) + 5.1
+z_output = np.dot(outputs_H, output_weights.T) + 5.1
 
 prediction = sigmoid(z_output)
 
 print(prediction)
 
 #backpropagation starts here
-loss = -(target*np.log(prediction) + (1-target)*np.log(1-prediction))
+loss = np.mean(-(target*np.log(prediction) + (1-target)*np.log(1-prediction)))
 
 error = prediction - target
 d_out = error * prediction * (1 - prediction)
@@ -52,8 +55,10 @@ d_output_weights = np.dot(outputs_H.T, d_out)
 
 blames_hidden = np.array(d_out[:, np.newaxis] * output_weights * outputs_H * (1 - outputs_H))
 
-blames_output = np.dot(blames_hidden.T, inputs)
+d_weights = np.dot(blames_hidden.T, inputs)
 
-weights -= learning_rate * blames_hidden
+weights -= learning_rate * d_weights
 
-output_weights -= learning_rate * blames_output
+output_weights -= learning_rate * d_output_weights
+
+np.savez('weights.npz', weights=weights, output_weights=output_weights, bias=bias)