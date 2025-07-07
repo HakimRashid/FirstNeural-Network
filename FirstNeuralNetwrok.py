831dcd04f4bd20c435cb3488be1936aec7f30451
HakimRashid <rashidhakim514@gmail.com>
2025-07-07 00:09:13 -0700

    learned: -input standardization: neural networks learn and predict much better when inputs are between 0 and 1 -weight initialization must start off not too big as it may lead to extremely z_outputs and extremely low sigmoid values during the learning process Fixed: -bias updating -z values not being re-calculated in the training loop Added: -simple interface to toggle between training and user input

--- FirstNeuralNetwrok.py
+++ FirstNeuralNetwrok.py
@@ -4,61 +4,91 @@ import matplotlib.pyplot as plt
 
 #file loading logic 
 if not os.path.exists('weights.npz'):
-    weights = np.array([[2.1, 5.4],
-                        [3.1, 6.4],
-                        [7.8, 9.1],
-                        [4, 0.4]])
-    output_weights = np.array([4.1, 2.1, 9.2, 7.8])
-    bias = np.array([1, 2, 3, 4])
-    np.savez('weights.npz', weights=weights, output_weights=output_weights, bias=bias)
+    weights = np.random.randn(4, 2) * 0.001
+    output_weights = np.random.randn(4) * 0.001
+    bias = np.zeros(4)
+    bias_output = 0.0
+    np.savez('weights.npz', weights=weights, output_weights=output_weights, bias=bias, bias_output=bias_output)
 data = np.load('weights.npz')
 weights = data['weights']
 output_weights = data['output_weights']
 bias = data['bias']
+bias_output = data['bias_output']
+training_inputs = np.array([[180.89, 70.83],
+                [165.23, 47.93],
+                [172.55, 60.19],
+                [158.76, 75.24],
+                [190.12, 91.56],
+                [170.05, 49.33],
+                [175.00, 75.87],
+                [162.19, 79.54],
+                [185.34, 82.01],
+                [153.45, 43.08]])
 
-
-inputs = np.array([[180.89, 70.83],
-        [165.23, 47.93],
-        [172.55, 60.19],
-        [158.76, 75.24],
-        [190.12, 91.56],
-        [170.05, 49.33],
-        [175.00, 75.87],
-        [162.19, 79.54],
-        [185.34, 82.01],
-        [153.45, 43.08]])
+train = input("Would you like to train me(Y/N)?")
+if train == 'Y':
+        training = True
+elif train == 'N':
+        training = False
+ 
+if training:
+        inputs = training_inputs
+else:
+        inputs = [float(input("\nEnter your height(cm):")), float(input("\nEnter your weight(kg):"))]
 target = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
-learning_rate = 0.01
-training = False
+learning_rate = 0.1
 
 #forwardpass begins here
+inputs = (inputs - np.min(training_inputs, axis=0)) / (np.max(training_inputs, axis=0) - np.min(training_inputs, axis=0))
+
 def sigmoid(x: float):
         return 1/(1+np.exp(-x))
 
 z_hidden = np.dot(inputs, weights.T) + bias
 
-outputs_H = np.array(sigmoid(z_hidden))
+outputs_H = sigmoid(z_hidden)
 
-z_output = np.dot(outputs_H, output_weights.T) + 5.1
+z_output = np.dot(outputs_H, output_weights) + bias_output
 
 prediction = sigmoid(z_output)
 
-print(prediction)
 
 #backpropagation starts here
-loss = np.mean(-(target*np.log(prediction) + (1-target)*np.log(1-prediction)))
 
-error = prediction - target
-d_out = error * prediction * (1 - prediction)
+if training:
+        for epoch in range(100000):
+                z_hidden = np.dot(inputs, weights.T) + bias
+                outputs_H = np.array(sigmoid(z_hidden))
+                z_output = np.dot(outputs_H, output_weights) + bias_output
+                prediction = sigmoid(z_output)
+                
+                prediction = np.clip(prediction, 1e-7, 1 - 1e-7)
+                loss = np.mean(-(target*np.log(prediction) + (1-target)*np.log(1-prediction)))
 
-d_output_weights = np.dot(outputs_H.T, d_out)
+                error = prediction - target
+                d_out = error * prediction * (1 - prediction)
+                d_output_weights = np.dot(outputs_H.T, d_out)
+                blames_hidden = np.array(d_out[:, np.newaxis] * output_weights * outputs_H * (1 - outputs_H))
+                d_weights = np.dot(blames_hidden.T, inputs)
+                d_bias = np.sum(blames_hidden, 0)
+                d_bias_output = np.sum(d_out)
 
-blames_hidden = np.array(d_out[:, np.newaxis] * output_weights * outputs_H * (1 - outputs_H))
+                weights -= learning_rate * d_weights
+                output_weights -= learning_rate * d_output_weights
+                bias -= learning_rate * d_bias
+                bias_output -= learning_rate * d_bias_output
+                
+                if epoch % 100 == 0:
+                        np.savez('weights.npz', weights=weights, output_weights=output_weights, bias=bias, bias_output=bias_output)
 
-d_weights = np.dot(blames_hidden.T, inputs)
-
-weights -= learning_rate * d_weights
-
-output_weights -= learning_rate * d_output_weights
-
-np.savez('weights.npz', weights=weights, output_weights=output_weights, bias=bias)
+#matlib plots
+if training:
+        plt.plot(prediction, label='Prediction')
+        plt.plot(target, label='Target')
+        plt.legend()
+        plt.show()
+else:
+        if round(prediction) == 1:
+                print("You are healthy!")
+        elif round(prediction) == 0:
+                print("You are not healthy!")
